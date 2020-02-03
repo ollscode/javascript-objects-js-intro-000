473a717cf28ff35cad20efa859b8a7d0f36da623
Oscar <mundonatural@gmail.com>
2020-02-03 14:55:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,3 +10,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
